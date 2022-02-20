81640d3b788d8416ed7a784ab87556ad37b7cc0a
Timotej03 <91258610+Timotej03@users.noreply.github.com>
2022-02-20 12:45:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ VYSKA = 500
 pozadie = biela
 hrac = pygame.transform.scale(pygame.image.load('doodle.png'), (60, 50))
 fps = 60
+cas = pygame.time.Clock()
+skore = 0
+najvyssie_skore = 0
+koniec_hry = False
+
 
 #FONT
 font = pygame.font.Font('freesansbold.ttf', 16)
 
-cas = pygame.time.Clock()
+
 
 #premenné
 hrac_x = 170
@@ -58,6 +63,7 @@ def pohyb_hraca(y_poz):
 
 #pohyb platforirm v priebehu hry
 def aktualizovane_ostrovceky(moj_list, y_poz, x_poz, zmena):
+    global skore
     if y_poz < 250 and zmena_y < 0:
         for i in range(len(moj_list)):
             moj_list[i][1] -= zmena
@@ -65,7 +71,9 @@ def aktualizovane_ostrovceky(moj_list, y_poz, x_poz, zmena):
             pass
         for item in range(len(moj_list)):
             if moj_list[item][i] > 500:
-                moj_list[item] = [random.randint(), 70, 10]
+                moj_list[item] = [random.randint(0, 330), random.randint(-50, -10), 70, 10]
+                skore += 1
+        return moj_list
 
 
 
@@ -75,6 +83,11 @@ while running == True:
     obrazovka.fill(pozadie)
     obrazovka.blit(hrac, (hrac_x, hrac_y))
     ostrovy = []
+    skore_text = font.render('NAJVYŠŠIE SKÓRE: ' + str(najvyssie_skore), True, cierna, pozadie)
+    obrazovka.blit(skore_text, (320, 20))
+    najvyssie_skore_text = font.render('SKÓRE: ' + str(skore), True, cierna, pozadie)
+    obrazovka.blit(najvyssie_skore_text, (280, 0))
+
 
     for i in range(len(ostrovceky)):
         ostrov = pygame.draw.rect(obrazovka, cierna, ostrovceky[i], 0, 4)
@@ -84,6 +97,14 @@ while running == True:
        if event.type == pygame.QUIT:
            running = False
        if event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_SPACE and koniec_hry:
+               koniec_hry = False
+               skore = 0
+               hrac_y = 400
+               hrac_x = 170
+               pozadie = biela
+               ostrovceky = [[165, 480, 70, 10], [85, 370, 70, 10], [265, 370, 70, 10], [165, 260, 70, 10],
+                             [85, 150, 70, 10], [265, 150, 70, 10], [265, 40, 70, 10]]
            if event.key == pygame.K_a:
                zmena_x = - rychlost_hraca
            if event.key == pygame.K_d:
@@ -96,9 +117,30 @@ while running == True:
 
     skok = kolizie(ostrovy, skok)
     hrac_x += zmena_x
-    hrac_y = pohyb_hraca(hrac_y)
+
+    if hrac_y < 440:
+        hrac_y = pohyb_hraca(hrac_y)
+    else:
+        koniec_hry = True
+        zmena_y = 0
+        zmena_x = 0
+
+
+
     ostrovceky = aktualizovane_ostrovceky(ostrovceky, hrac_x, hrac_y)
 
+    if hrac_x < -20:
+        hrac_x = -20
+    elif hrac_x > 330:
+        hrac_x = 330
+
+    if zmena_x > 0:
+        hrac = pygame.transform.scale(pygame.image.load('doodle.png'), (60, 50))
+    elif zmena_x < 0:
+        hrac = pygame.transform.flip(pygame.transform.scale(pygame.image.load('doodle.png'), (60, 50)), 1, 0)
+
+    if skore > najvyssie_skore:
+        najvyssie_skore = skore
 
     pygame.display.flip()
 pygame.quit()
